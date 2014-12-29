61fcdbb8a4629df9ec639ac9549addf3c32aa498
David Meister <thedavidmeister@gmail.com>
2014-12-29 14:41:34 +1100

    working on tests for rng

--- src/Foo/RandomBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Foo/RandomBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,17 +8,17 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        // $client = static::createClient();
 
-        $i = 1;
-        $j = 512;
+        // $i = 1;
+        // $j = 512;
 
-        while ($i < $j) {
-          $crawler = $client->request('GET', '/random/' . $i);
-          print $client->getResponse()->getContent() . '|';
-          $this->assertTrue(mb_strlen(json_decode($client->getResponse()->getContent(), TRUE)) === $i);
-          $i++;
-        }
+        // while ($i < $j) {
+        //   $crawler = $client->request('GET', '/random/' . $i);
+        //   print $client->getResponse()->getContent() . '|';
+        //   // $this->assertTrue(mb_strlen(json_decode($client->getResponse()->getContent(), TRUE)) === $i);
+        //   $i++;
+        // }
 
 
     }
